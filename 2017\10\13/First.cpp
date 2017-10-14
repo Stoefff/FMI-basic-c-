5fd148836757189fdfcf0a866c37db2e8045d5b7
Stoefff <xxthespinerxx@gmail.com>
2017-10-14 21:49:00 +0300

    Friday exercise

---
+++ 2017\10\13/First.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int numberOfFucks;
+    std::cout << "I would like to receive a number" << endl;
+    std::cin >> numberOfFucks;
+    for(int i = 320000; i <= numberOfFucks; i++){
+        std::cout << "Syka Blqd" << endl;
+        std::cout << i << endl;
+    }
+    return 0;
+}
