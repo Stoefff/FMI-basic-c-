6f3f8bb4365ffe49f24015c13ca5170a16e3e8ab
Stoefff <xxthespinerxx@gmail.com>
2017-11-08 11:30:25 +0200

    Added some binary ops tasks

---
+++ 2017\11\03/ETask.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+
+    unsigned int getter, setter, number;
+    unsigned mask = 1;
+    bool value;
+
+    std::cout << "Enter number: " << std::endl;
+    std::cin >> number;
+
+    std::cout << "Enter index to get value"  << std::endl;
+    std::cin >> getter;
+
+    std::cout << "Enter index to set value" << std::endl;
+    std::cin >> setter;
+
+    value = bool(number & (mask << getter));
+    number = number & (~(value << getter )
+    return 0;
+}
