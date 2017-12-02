44eef77910edbf3bd6fed3c6ad95dedf9102dc25
Stoefff <xxthespinerxx@gmail.com>
2017-12-02 22:38:04 +0200

    Mostly nothing

---
+++ Week6/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+    unsigned int number;
+    unsigned int mask = (1 << 31);
+    std::cin >> number;
+    for(int i = 31; i >= 0; i--){
+        std::cout << (bool(number & mask));
+        mask >>= 1;
+    }
+    std::cout << std::endl;
+    mask = mask++;
+    return 0;
+
+}
+
+//10101010101010101010101
+//&
+//10000000000000000000000
+//=
+//10000000000000000000000
